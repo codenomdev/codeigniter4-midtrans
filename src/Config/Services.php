4457c75e8ac49c3ff550a2476a4a712f7dd0a185
codenomdev <falachul14@gmail.com>
2020-11-27 01:27:57 +0700

    Add description on file php
    Add description on file php

--- src/Config/Services.php
+++ src/Config/Services.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @see       https://github.com/codenomdev/codeigniter4-midtrans for the canonical source repository
+ * @copyright 2020 - Codenom Dev (https://codenom.com).
+ * @license   https://github.com/codenomdev/codeigniter4-midtrans/blob/main/LICENSE MIT License
+ */
+
 namespace Codenom\Midtrans\Config;
 
 use CodeIgniter\Config\Services as CoreServices;
